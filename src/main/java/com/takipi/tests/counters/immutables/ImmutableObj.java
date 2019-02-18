0c7897aa27ad38e7d030cb9b78760c4164c96a3d
samuel cardonis <o528060707@gmail.com>
2019-02-18 12:26:34 +0200

    immutaqble

--- src/main/java/com/takipi/tests/counters/immutables/ImmutableObj.java
+++ src/main/java/com/takipi/tests/counters/immutables/ImmutableObj.java
@@ -5,22 +5,29 @@ import com.google.common.collect.ImmutableList;
 
 /*
 An immutable object is an object whose internal state remains constant after it has been entirely created.
+1. make class final
+2. make all fields final and private
+3. replace void setXXX(value)  methods  with ClassXXX setXXX(value) that create copy of ClassXXX
+4. copy mutable fields on constructor
+5. recommended to use immutable collections or scala/kotlin
+6. or libraries with annotations
+* 1. https://github.com/immutables/immutables
+* 2. https://github.com/rzwitserloot/lombok
+* 3. autovalue
+
 
 This means that :
  1. public API of an immutable object guarantees us that it will behave in the same way during its whole lifetime.
- 2. thread safe
- 3. easily scalable
+ 2. thread safe/no side effects/avoid coupling/avoid shared mutability
+ 3. easier scale
+ 4. easier to test
 
  but it has some disadvantages:
- 1. too many new resources ar egenerated
+ 1. too many new resources are generated
 
- so do not use on write intessive app.
+ so do not use on write intensive app.
 
-* u can use:
-* 1. https://github.com/immutables/immutables
-* 2. https://github.com/rzwitserloot/lombok
-* 3. use scala/kotlin
-* 4. your own implemantation using final class and private final fields
+
 * */
 public final class ImmutableObj {
 
